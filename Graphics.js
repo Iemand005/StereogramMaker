a1dfedc3f8b517353160fef635ed6395f4b3e002
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-04-08 19:16:21 +0200

    closer but its blaack

--- Graphics.js
+++ Graphics.js
@@ -116,12 +116,14 @@ let then = 0;
 
 Graphics3D.prototype.drawScene = function (programInfo, deltaTime) {
   const gl = this.gl;
+  if (!gl) return;
 
   gl.clearDepth(1.0); // Clear everything
   gl.enable(gl.DEPTH_TEST); // Enable depth testing
   gl.depthFunc(gl.LEQUAL); // Near things obscure far things
 
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.clearDepth(1.0);
 
   const fov = 45;
 
--- index.js
+++ index.js
@@ -155,8 +155,15 @@ const canvas = document.getElementById("canvas");
 if (canvas instanceof HTMLCanvasElement) {
     const vsSource =
     "attribute vec4 aVertexPosition; attribute vec4 aVertexColor; uniform mat4 uModelViewMatrix; uniform mat4 uProjectionMatrix; varying lowp vec4 vColor; void main() { gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition; vColor = aVertexColor; }";
-    const fsSource =
+    const fsSourceColour =
     "varying lowp vec4 vColor; void main() { gl_FragColor = vColor; }";
+    const fsSource = `
+    precision lowp float;
+    void main() {
+        float depth = 1.0 - gl_FragCoord.z;
+        gl_FragColor = vec4(vec3(depth), 1.0);
+    }
+`;
 
     const graphics = new Graphics3D(canvas);
     const gl = graphics.gl;
