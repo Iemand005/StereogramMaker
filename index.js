2735f588b6d693b0520d667bbd68d27ea657bb31
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-04-08 04:13:38 +0200

    clenaups

--- index.js
+++ index.js
@@ -19,65 +19,9 @@ uniform sampler2D iChannel0;
 uniform sampler2D iChannel1;
 varying vec2 vUv;
 
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
-{
-    float numTiles = 6.0;
-    float maxDepthShift = 20.0;
-    
-    float tileWidth = iResolution.x / numTiles;
-    
-    float currX = fragCoord.x;
-    float y = fragCoord.y;
-
-    // Normalized pixel coordinates (from 0 to 1)
-    vec2 uv = fragCoord / iResolution.xy;
-
-    // Get depth value for pixel
-    float depth = texture2D(iChannel1, uv).r;
-    
-    // while (currX > tileWidth) {
-    for (int i = 0; i < 30; i++) {
-        // Get depth at the current spot (0.0 to 1.0)
-        float depth = texture2D(iChannel1, vec2(currX, y) / iResolution.xy).r;
-        
-        // Subtract the tile width, but shift it slightly based on depth
-        // More depth (whiter) = smaller jump = pixels look "closer"
-        currX -= (tileWidth - depth * maxDepthShift);
-    }
-    
-    
-    // Get the colour of the offset texture coordinates
-    vec4 col = texture2D(iChannel1, uv + vec2(depth * 0.1, 0.0));
-    
-
-    // Output to screen
-    vec2 patternUV = vec2(currX / tileWidth, y / iResolution.y);
-    fragColor = texture2D(iChannel0, patternUV);
-    // fragColor
-}
-
-// void main() {
-//     // mainImage(gl_FragColor, gl_FragCoord.xy);
-//     // vec4 col = texture2D(iChannel0, vUv);
-
-
-//     for (int i = 0; i < 30; i++) {
-//         // Get depth at the current spot (0.0 to 1.0)
-//         float depth = texture2D(iChannel1, vec2(currX, y) / iResolution.xy).r;
-        
-//         // Subtract the tile width, but shift it slightly based on depth
-//         // More depth (whiter) = smaller jump = pixels look "closer"
-//         currX -= (tileWidth - depth * maxDepthShift);
-//     }
-    
-
-//     gl_FragColor = col;
-//     // gl_FragColor = texture2D(iChannel0, vUv);
-// }
-
 void main() {
     // 1. Settings
-    float numTiles = 6.0; 
+    float numTiles = 6.0;
     float maxDepthShift = 20.0; // Adjust this for more/less 3D pop
     float tileWidth = iResolution.x / numTiles;
     
@@ -138,31 +82,25 @@ const posLoc = gl.getAttribLocation(program, "position");
 gl.enableVertexAttribArray(posLoc);
 gl.vertexAttribPointer(posLoc, 2, gl.FLOAT, false, 0, 0);
 
-// Set Resolution Uniform
 const resLoc = gl.getUniformLocation(program, "iResolution");
 gl.uniform2f(resLoc, canvas.width, canvas.height);
 
-// --- TEXTURE LOADING HELPER ---
 function loadTexture(url, index) {
   gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true); 
     const tex = gl.createTexture();
     gl.activeTexture(gl.TEXTURE0 + index);
     gl.bindTexture(gl.TEXTURE_2D, tex);
     
-    // Set wrapping to REPEAT so the pattern tiles
-    // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
-    // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
-    // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 
     const img = new Image();
-    img.onload = () => {
+    img.onload = function() {
         gl.activeTexture(gl.TEXTURE0 + index);
         gl.bindTexture(gl.TEXTURE_2D, tex);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
-        draw(); // Draw once the image is ready
+        draw();
     };
     img.crossOrigin = "anonymous";
     img.src = url;
@@ -170,11 +108,9 @@ function loadTexture(url, index) {
     return tex;
 }
 
-// Set Texture Uniforms
 gl.uniform1i(gl.getUniformLocation(program, "iChannel0"), 0);
 gl.uniform1i(gl.getUniformLocation(program, "iChannel1"), 1);
 
-// Load your local images here
 loadTexture('image.png', 0); 
 loadTexture('shark.png', 1);
 
