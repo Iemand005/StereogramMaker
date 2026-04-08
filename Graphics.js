36f22b9b4b44b3610ec2b7018138d93983701ff7
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-04-08 18:48:43 +0200

    let t be

--- Graphics.js
+++ Graphics.js
@@ -1,12 +1,6 @@
-/*const*/var canvas = document.getElementById("canvas");
-
-/*const*/var vsSource =
-  "attribute vec4 aVertexPosition; attribute vec4 aVertexColor; uniform mat4 uModelViewMatrix; uniform mat4 uProjectionMatrix; varying lowp vec4 vColor; void main() { gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition; vColor = aVertexColor; }";
-/*const*/var fsSource =
-  "varying lowp vec4 vColor; void main() { gl_FragColor = vColor; }";
 
 Graphics.prototype.loadShader = function (type, source) {
-  /*const*/var shader = this.gl.createShader(type);
+  const shader = this.gl.createShader(type);
   this.gl.shaderSource(shader, source);
   this.gl.compileShader(shader);
 
@@ -25,12 +19,12 @@ Graphics.prototype.loadShader = function (type, source) {
 };
 
 Graphics.prototype.initShaderProgram = function (vsSource, fsSource) {
-  /*const*/var vertexShader = this.loadShader(this.gl.VERTEX_SHADER, vsSource);
-  /*const*/var fragmentShader = this.loadShader(this.gl.FRAGMENT_SHADER, fsSource);
+  const vertexShader = this.loadShader(this.gl.VERTEX_SHADER, vsSource);
+  const fragmentShader = this.loadShader(this.gl.FRAGMENT_SHADER, fsSource);
 
   // Create the shader program
 
-  /*const*/var shaderProgram = this.gl.createProgram();
+  const shaderProgram = this.gl.createProgram();
   this.gl.attachShader(shaderProgram, vertexShader);
   this.gl.attachShader(shaderProgram, fragmentShader);
   this.gl.linkProgram(shaderProgram);
@@ -77,28 +71,28 @@ Graphics.prototype.clear = function () {
 };
 
 Graphics.prototype.loadShaders = function (vsSource, fsSource) {
-  /*const*/var gl = this.gl;
+  const gl = this.gl;
   this.shaderProgram = this.initShaderProgram(vsSource, fsSource);
 
-  /*const*/var positionBuffer = gl.createBuffer();
+  const positionBuffer = gl.createBuffer();
 
   // Select the positionBuffer as the one to apply buffer
   // operations to from here out.
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
   // Now create an array of positions for the square.
-  /*const*/var positions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];
+  const positions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];
 
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 };
 
-/*let*/var squareRotation = 0.0;
-/*let*/var deltaTime = 0;
-// /*let*/var now = 0;
-/*let*/var then = 0;
+let squareRotation = 0.0;
+let deltaTime = 0;
+// let now = 0;
+let then = 0;
 
 Graphics.prototype.drawScene = function (programInfo, deltaTime) {
-  /*const*/var gl = this.gl;
+  const gl = this.gl;
 
   gl.clearDepth(1.0); // Clear everything
   gl.enable(gl.DEPTH_TEST); // Enable depth testing
@@ -106,13 +100,13 @@ Graphics.prototype.drawScene = function (programInfo, deltaTime) {
 
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-  /*const*/var fov = 45;
+  const fov = 45;
 
-  /*const*/var fieldOfView = (fov * Math.PI) / 180; // in radians
-  /*const*/var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-  /*const*/var zNear = 0.1;
-  /*const*/var zFar = 100.0;
-  /*const*/var projectionMatrix = mat4.create();
+  const fieldOfView = (fov * Math.PI) / 180; // in radians
+  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+  const zNear = 0.1;
+  const zFar = 100.0;
+  const projectionMatrix = mat4.create();
 
   // note: glMatrix always has the first argument
   // as the destination to receive the result.
@@ -121,7 +115,7 @@ Graphics.prototype.drawScene = function (programInfo, deltaTime) {
 
   // Set the drawing position to the "identity" point, which is
   // the center of the scene.
-  /*const*/var modelViewMatrix = mat4.create();
+  const modelViewMatrix = mat4.create();
 
   squareRotation += deltaTime;
   mat4.translate(
@@ -152,12 +146,12 @@ Graphics.prototype.drawScene = function (programInfo, deltaTime) {
   // Tell WebGL how to pull out the positions from the position
   // buffer into the vertexPosition attribute.
   {
-    /*const*/var numComponents = 3; // pull out 3 values per iteration (x, y, z)
-    /*const*/var type = gl.FLOAT; // the data in the buffer is 32bit floats
-    /*const*/var normalize = false; // don't normalize
-    /*const*/var stride = 0; // how many bytes to get from one set of values to the next
+    const numComponents = 3; // pull out 3 values per iteration (x, y, z)
+    const type = gl.FLOAT; // the data in the buffer is 32bit floats
+    const normalize = false; // don't normalize
+    const stride = 0; // how many bytes to get from one set of values to the next
     // 0 = use type and numComponents above
-    /*const*/var offset = 0; // how many bytes inside the buffer to start from
+    const offset = 0; // how many bytes inside the buffer to start from
     if (!this.buffers) return;
     gl.bindBuffer(gl.ARRAY_BUFFER, this.buffers.position);
     gl.vertexAttribPointer(
@@ -171,11 +165,11 @@ Graphics.prototype.drawScene = function (programInfo, deltaTime) {
     gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
   }
 
-  /*const*/var numComponents = 4;
-  /*const*/var type = gl.FLOAT;
-  /*const*/var normalize = false;
-  /*const*/var stride = 0;
-  /*const*/var offset = 0;
+  const numComponents = 4;
+  const type = gl.FLOAT;
+  const normalize = false;
+  const stride = 0;
+  const offset = 0;
   gl.bindBuffer(gl.ARRAY_BUFFER, this.buffers.color);
   gl.vertexAttribPointer(
     programInfo.attribLocations.vertexColor,
@@ -203,13 +197,13 @@ Graphics.prototype.drawScene = function (programInfo, deltaTime) {
   );
 
   {
-    // /*const*/var offset = 0;
-    // /*const*/var vertexCount = 4;
+    // const offset = 0;
+    // const vertexCount = 4;
     // gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
 
-    /*const*/var vertexCount = 36;
-    /*const*/var type = gl.UNSIGNED_SHORT;
-    /*const*/var offset = 0;
+    const vertexCount = 36;
+    const type = gl.UNSIGNED_SHORT;
+    const offset = 0;
     gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
   }
 };
@@ -236,7 +230,7 @@ Graphics.prototype.startRendering = function () {
 };
 
 Graphics.prototype.resize = function (width, height) {
-  /*const*/var dpr = window.devicePixelRatio || 1;
+  const dpr = window.devicePixelRatio || 1;
   this.canvas.width = width * dpr;
   this.canvas.height = height * dpr;
   this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
@@ -245,129 +239,3 @@ Graphics.prototype.resize = function (width, height) {
 };
 
 // export { drawScene };
-
-/*const*/var graphics = new Graphics(canvas);
-/*const*/var gl = graphics.gl;
-graphics.clear();
-graphics.loadShaders(vsSource, fsSource);
-
-/*const*/var programInfo = {
-  program: graphics.shaderProgram,
-  attribLocations: {
-    vertexPosition: gl.getAttribLocation(
-      graphics.shaderProgram,
-      "aVertexPosition"
-    ),
-    vertexColor: gl.getAttribLocation(graphics.shaderProgram, "aVertexColor")
-  },
-  uniformLocations: {
-    projectionMatrix: gl.getUniformLocation(
-      graphics.shaderProgram,
-      "uProjectionMatrix"
-    ),
-    modelViewMatrix: gl.getUniformLocation(
-      graphics.shaderProgram,
-      "uModelViewMatrix"
-    )
-  }
-};
-
-/*const*/var positionBuffer = gl.createBuffer();
-
-// Select the positionBuffer as the one to apply buffer
-// operations to from here out.
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-// Now create an array of positions for the square.
-// /*const*/var positions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];y
-
-/*const*/var positions = [
-  // Front face
-  -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0,
-
-  // Back face
-  -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0,
-
-  // Top face
-  -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0,
-
-  // Bottom face
-  -1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0,
-
-  // Right face
-  1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0,
-
-  // Left face
-  -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0
-];
-
-// Now pass the list of positions into WebGL to build the
-// shape. We do this by creating a Float32Array from the
-// JavaScript array, then use it to fill the current buffer.
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-/*const*/var indexBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-
-// This array defines each face as two triangles, using the
-// indices into the vertex array to specify each triangle's
-// position.
-
-// prettier-ignore
-/*const*/var indices = [
-  0, 1, 2, 0, 2, 3,    // front
-  4, 5, 6, 4, 6, 7,    // back
-  8, 9, 10, 8, 10, 11,   // top
-  12, 13, 14, 12, 14, 15,   // bottom
-  16, 17, 18, 16, 18, 19,   // right
-  20, 21, 22, 20, 22, 23   // left
-];
-
-/*const*/var faceColors = [
-  [1.0, 1.0, 1.0, 1.0], // Front face: white
-  [1.0, 0.0, 0.0, 1.0], // Back face: red
-  [0.0, 1.0, 0.0, 1.0], // Top face: green
-  [0.0, 0.0, 1.0, 1.0], // Bottom face: blue
-  [1.0, 1.0, 0.0, 1.0], // Right face: yellow
-  [1.0, 0.0, 1.0, 1.0] // Left face: purple
-];
-
-// Convert the array of colors into a table for all the vertices.
-
-/*let*/var colors = [];
-
-for (/*let*/var cIndex in faceColors) {
-  // Repeat each color four times for the four vertices of the face
-  /*const*/var c = faceColors[cIndex];
-  colors = colors.concat(c, c, c, c);
-}
-
-/*const*/var colorBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-
-/*const*/var buffers = {
-  position: positionBuffer,
-  indices: indexBuffer,
-  color: colorBuffer
-};
-
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
-gl.bufferData(
-  gl.ELEMENT_ARRAY_BUFFER,
-  new Uint16Array(indices),
-  gl.STATIC_DRAW
-);
-
-graphics.buffers = buffers;
-
-/*const*/var bounds = canvas.getBoundingClientRect();
-graphics.resize(bounds.width, bounds.height);
-graphics.startRendering();
-
-console.log(graphics);
-
-window.onresize = function (ev) {
-  /*const*/var bounds = canvas.getBoundingClientRect();
-  graphics.resize(bounds.width, bounds.height);
-};
--- index.js
+++ index.js
@@ -150,3 +150,138 @@ function init() {
 init();
 
 
+
+const canvas = document.getElementById("canvas");
+if (canvas instanceof HTMLCanvasElement) {
+    const vsSource =
+    "attribute vec4 aVertexPosition; attribute vec4 aVertexColor; uniform mat4 uModelViewMatrix; uniform mat4 uProjectionMatrix; varying lowp vec4 vColor; void main() { gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition; vColor = aVertexColor; }";
+    const fsSource =
+    "varying lowp vec4 vColor; void main() { gl_FragColor = vColor; }";
+
+    const graphics = new Graphics(canvas);
+    const gl = graphics.gl;
+    graphics.clear();
+    graphics.loadShaders(vsSource, fsSource);
+
+    const programInfo = {
+    program: graphics.shaderProgram,
+    attribLocations: {
+        vertexPosition: gl.getAttribLocation(
+        graphics.shaderProgram,
+        "aVertexPosition"
+        ),
+        vertexColor: gl.getAttribLocation(graphics.shaderProgram, "aVertexColor")
+    },
+    uniformLocations: {
+        projectionMatrix: gl.getUniformLocation(
+        graphics.shaderProgram,
+        "uProjectionMatrix"
+        ),
+        modelViewMatrix: gl.getUniformLocation(
+        graphics.shaderProgram,
+        "uModelViewMatrix"
+        )
+    }
+    };
+
+    const positionBuffer = gl.createBuffer();
+
+    // Select the positionBuffer as the one to apply buffer
+    // operations to from here out.
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    // Now create an array of positions for the square.
+    // const positions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];y
+
+    const positions = [
+    // Front face
+    -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0,
+
+    // Back face
+    -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0,
+
+    // Top face
+    -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0,
+
+    // Bottom face
+    -1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0,
+
+    // Right face
+    1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0,
+
+    // Left face
+    -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0
+    ];
+
+    // Now pass the list of positions into WebGL to build the
+    // shape. We do this by creating a Float32Array from the
+    // JavaScript array, then use it to fill the current buffer.
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    const indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+    // This array defines each face as two triangles, using the
+    // indices into the vertex array to specify each triangle's
+    // position.
+
+    // prettier-ignore
+    const indices = [
+    0, 1, 2, 0, 2, 3,    // front
+    4, 5, 6, 4, 6, 7,    // back
+    8, 9, 10, 8, 10, 11,   // top
+    12, 13, 14, 12, 14, 15,   // bottom
+    16, 17, 18, 16, 18, 19,   // right
+    20, 21, 22, 20, 22, 23   // left
+    ];
+
+    const faceColors = [
+    [1.0, 1.0, 1.0, 1.0], // Front face: white
+    [1.0, 0.0, 0.0, 1.0], // Back face: red
+    [0.0, 1.0, 0.0, 1.0], // Top face: green
+    [0.0, 0.0, 1.0, 1.0], // Bottom face: blue
+    [1.0, 1.0, 0.0, 1.0], // Right face: yellow
+    [1.0, 0.0, 1.0, 1.0] // Left face: purple
+    ];
+
+    // Convert the array of colors into a table for all the vertices.
+
+    let colors = [];
+
+    for (let cIndex in faceColors) {
+    // Repeat each color four times for the four vertices of the face
+    const c = faceColors[cIndex];
+    colors = colors.concat(c, c, c, c);
+    }
+
+    const colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+    const buffers = {
+    position: positionBuffer,
+    indices: indexBuffer,
+    color: colorBuffer
+    };
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
+    gl.bufferData(
+    gl.ELEMENT_ARRAY_BUFFER,
+    new Uint16Array(indices),
+    gl.STATIC_DRAW
+    );
+
+    graphics.buffers = buffers;
+
+    const bounds = canvas.getBoundingClientRect();
+    graphics.resize(bounds.width, bounds.height);
+    graphics.startRendering();
+
+    console.log(graphics);
+
+    window.onresize = function (ev) {
+    const bounds = canvas.getBoundingClientRect();
+    graphics.resize(bounds.width, bounds.height);
+    };
+
+}
